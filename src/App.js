2824c3b2d5602625e70ffd2bd2cc2c8f5b32d1b5
zencam@imac <zen.camera.project@gmail.com>
2018-01-31 18:07:19 -0500

    Props example.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">
+        	Attribute "someprop" is rendered as: {this.props.someprop}!
+	</p>
       </div>
     );
   }
