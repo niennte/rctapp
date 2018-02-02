5df146db34b724e2ca07ccae253648bfb7e3d434
zencam@imac <zen.camera.project@gmail.com>
2018-02-02 17:46:02 -0500

    Add sorting.

--- src/ProductTable.js
+++ src/ProductTable.js
@@ -14,16 +14,33 @@ class ProductTable extends Component {
                 direction: 'desc'
             }
         };
+
+        this.sortByColumnAndDirection = this.sortByColumnAndDirection.bind(this);
     }
 
+    sortByColumnAndDirection(objectA, objectB) {
+        let isDesc = this.state.sort.direction === 'desc' ? -1 : 1;
+        let [a, b] = [objectA[this.state.sort.column], objectB[this.state.sort.column]];
+        if (this.state.sort.column === 'price') {
+            [a, b] = [a, b].map((value) => parseFloat(value.replace(/[^\d\.]/g, ''), 10));
+        }
+        if (a > b) {
+            return 1 * isDesc;
+        }
+        if (a < b) {
+            return -1 * isDesc;
+        }
+        return 0;
+    }
+
+    sortProducts() {
+        let productArray = Object.keys(this.props.products).map((pid) => this.props.products[pid]);
+        return productArray.sort(this.sortByColumnAndDirection);         
+    }
 
     render() {
-        
-        let productArray = Object.keys(this.props.products)
-                .map((pid) => this.props.products[pid]);
-
         let rows = [];
-        productArray.forEach(
+        this.sortProducts().forEach(
             (product) => {
                 if ( product.name.indexOf(this.props.filterText) === -1 ||
                     (!product.stocked && this.props.inStockOnly)) {
@@ -41,9 +58,15 @@ class ProductTable extends Component {
                 <table className="ProductTable">
                     <thead>
                         <tr>
-                            <ProductTableHeader column="name">
+                            <ProductTableHeader 
+                                column="name"
+                                currentSort={this.state.sort}
+                            >
                             </ProductTableHeader>
-                            <ProductTableHeader column="price">
+                            <ProductTableHeader
+                                column="price"
+                                currentSort={this.state.sort}
+                            >
                             </ProductTableHeader>
                         </tr>
                     </thead>
